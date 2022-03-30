64041ca82d928d1656ca27160297c02a832f4000
Meresa <meresa27@gmail.com>
2022-03-30 10:18:07 -0500

    fixed app listening to server.address().port

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const { Server } = require('http');
 
 const app = express();
 
 dotenv.config({path : './config/.env'});
 
-app.use("/public", express.static(path.join(__dirname,
+app.use(express.static(path.join(__dirname,
     "public")))
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, "app.js"));
-})
+// app.get('/', (req, res) => {
+//     res.status(200).sendFile(path.join(__dirname, "app.js"));
+// })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on ${process.env.PORT}`);
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server running on ${server.address().port}`);
 })
