04c8284b6f798a2efa59ffb32cf62259be160820
Meresa <meresa27@gmail.com>
2022-04-02 18:06:18 -0500

    add api hardening for review controller

--- api/controllers/reviewController.js
+++ api/controllers/reviewController.js
@@ -3,18 +3,46 @@ const mongoose = require('mongoose');
 const Game = mongoose.model(process.env.GAME_MODEL);
 
 const getOne = (req, res) => {
-    console.log("GET One Publisher controller");
 
-    const gameId = req.params.gameId;
+    const gameId= req.params.gameId;
     const reviewId = req.params.reviewId;
 
-    Game.findById(gameId).select('reviews').exec(function(err, game){
+    const response = {
+        status : process.env.HTTP_STATUS_OK,
+        message : {} 
+    }
+    //check if gameId is valid
+    if(!(mongoose.isValidObjectId(gameId) && mongoose.isValidObjectId(reviewId))) {
+            console.log("invalid Id");
+            response.status = process.env.HTTP_STATUS_NOTFOUND;
+            response.message = {message : "Invalid gameId or reviewId"}
+            return res.status(response.status).json(response.message)
+    }
+
+    Game.findById(gameId).select('reviews').exec(function(err, games) {
         if(err) {
-            return res.status(500).json({error : err})
+            console.log("Error reading game");
+            response.status = process.env.HTTP_STATUS_INTERNAL_ERROR;
+            response.message = {error : err};
+
+        } 
+        else {
+            if(games) {
+                console.log("Found Game Review");
+                response.status = process.env.HTTP_STATUS_OK;
+                response.message = games.reviews.id(reviewId);
+            }
+            else {
+                console.log("game is null");
+                response.status = process.env.HTTP_STATUS_NOTFOUND;
+                response.message = {message : "Game with given Id not found"};
+            }
         }
-        console.log("Game Found", game);
-        return res.status(200).json(game.reviews.id(reviewId))
-    })
+
+        return res.status(response.status).json(response.message);
+        
+        });
+
 } 
 
 const getAll = (req, res) => {
@@ -22,13 +50,41 @@ const getAll = (req, res) => {
 
     const gameId = req.params.gameId;
 
-    Game.findById(gameId).select('reviews').exec(function(err, game){
+    const response = {
+        status : process.env.HTTP_STATUS_OK,
+        message : {} 
+    }
+    //check if gameId is valid
+    if(!mongoose.isValidObjectId(gameId)) {
+            console.log("invalid Id");
+            response.status = process.env.HTTP_STATUS_NOTFOUND;
+            response.message = {message : "Invalid gameId or reviewId"}
+            return res.status(response.status).json(response.message)
+    }
+
+    Game.findById(gameId).select('reviews').exec(function(err, games) {
         if(err) {
-            return res.status(500).json({error : err})
+            console.log("Error reading game");
+            response.status = process.env.HTTP_STATUS_INTERNAL_ERROR;
+            response.message = {error : err};
+
+        } 
+        else {
+            if(games) {
+                console.log("Found Game Review");
+                response.status = process.env.HTTP_STATUS_OK;
+                response.message = games.reviews;
+            }
+            else {
+                console.log("game is null");
+                response.status = process.env.HTTP_STATUS_NOTFOUND;
+                response.message = {message : "Game with given Id not found"};
+            }
         }
-        console.log("Game Found", game);
-        return res.status(200).json(game.reviews)
-    })
+
+        return res.status(response.status).json(response.message);
+        
+        });
 } 
 
 module.exports = {
