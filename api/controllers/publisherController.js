c2d22da9b114bfa9e8c8c0ac52deed69a7638f73
Meresa <meresa27@gmail.com>
2022-04-02 17:56:46 -0500

    add api hardening for publisher controller

--- api/controllers/publisherController.js
+++ api/controllers/publisherController.js
@@ -4,16 +4,42 @@ const Game = mongoose.model(process.env.GAME_MODEL);
 
 const getOne = (req, res) => {
     console.log("GET One Publisher controller");
+    const gameId= req.params.gameId;
+    const response = {
+        status : process.env.HTTP_STATUS_OK,
+        message : {} 
+    }
+    //check if gameId is valid
+    if(!mongoose.isValidObjectId(gameId)) {
+            console.log("invalid Id");
+            response.status = process.env.HTTP_STATUS_NOTFOUND;
+            response.message = {message : "Invalid gameId"}
+            return res.status(response.status).json(response.message)
+    }
 
-    const gameId = req.params.gameId;
-
-    Game.findById(gameId).select('publisher').exec(function(err, game){
+    Game.findById(gameId).select('publisher').exec(function(err, games) {
         if(err) {
-            res.status(500).json({eror : err})
+            console.log("Error reading game");
+            response.status = process.env.HTTP_STATUS_INTERNAL_ERROR;
+            response.message = {error : err};
+
+        } 
+        else {
+            if(games) {
+                console.log("Found Game");
+                response.status = process.env.HTTP_STATUS_OK;
+                response.message = games.publisher;
+            }
+            else {
+                console.log("game is null");
+                response.status = process.env.HTTP_STATUS_NOTFOUND;
+                response.message = {message : "Game with given Id not found"};
+            }
         }
-        console.log("Game Found", game);
-        res.status(200).json(game.publisher)
-    })
+
+        return res.status(response.status).json(response.message);
+        
+        });
 } 
 
 module.exports = {
