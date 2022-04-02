38a924d99105c26440a28c7dd5f0f35411c355df
Meresa <meresa27@gmail.com>
2022-04-02 10:14:45 -0500

    add rest api return status regardless

--- api/controllers/publisherController.js
+++ api/controllers/publisherController.js
@@ -8,6 +8,9 @@ const getOne = (req, res) => {
     const gameId = req.params.gameId;
 
     Game.findById(gameId).select('publisher').exec(function(err, game){
+        if(err) {
+            res.status(500).json({eror : err})
+        }
         console.log("Game Found", game);
         res.status(200).json(game.publisher)
     })
